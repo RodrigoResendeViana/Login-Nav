049a842e64079ab2426273d4d36139483a9d47d5
Logon Aluno <logonrmlocal@fiap.com.br>
2023-11-13 11:54:20 -0300

    Login com o nome na nav

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { } from 'react'
+import { Outlet } from 'react-router-dom'
+import Nav from './components/Nav'
+
+
+function App() {
+  return (
+    <>
+      <Nav/>
+      <Outlet/>
+    </>
+  )
+}
+
+export default App
---
+++ src/routers/Home.jsx
@@ -0,0 +1,12 @@
+import { } from 'react'
+
+
+function Home() {
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default Home
